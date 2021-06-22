8be2c2777bec389d64e2ecc97d3dead6a555d629
chrisengel3 <chrisengel3@gmail.com>
2021-06-22 11:36:07 -0700

    W1D2 Styling Finished

--- Week_1/Algos/W1D2.js
+++ Week_1/Algos/W1D2.js
@@ -1,2 +1,19 @@
 // Variables
-var myName = "Chris"
+var myName = "Chris" //String
+var myAge = "28" //also string
+var Age = 28 //integer 
+
+var sleepy = true //Boolean (true or false)
+var fruits = ["Apple", "Pear", "Strawberry"] //Array call with index fruits[#]
+var weight = 165.5 //Floats (decimal numbers)
+
+//Loops For and While
+
+var num = 1
+console.log(num)
+
+for(var num = 1; num < 100; num++){
+    console.log(num)
+}
+
+function getTotal(arrayOfNumbers)
