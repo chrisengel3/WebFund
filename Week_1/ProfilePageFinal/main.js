0037b4d787e2324615787eebcfa7fbf448f1ab19
chrisengel3 <chrisengel3@gmail.com>
2021-06-28 16:06:36 -0700

    W2D2 Making Connections

---
+++ Week_1/ProfilePageFinal/main.js
@@ -0,0 +1,9 @@
+function clearRequest(){
+    var request = document.querySelector(".otheruseryn")
+    request.remove()
+    console.log("yes")
+}
+function nameChange(){
+    var name = document.querySelector("#name")
+    name.innerText = "Engel Chris"
+}
