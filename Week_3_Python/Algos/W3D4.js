9802f1614ca84e28b016f9d2d6672141e5ca915f
chrisengel3 <chrisengel3@gmail.com>
2021-07-08 10:07:50 -0700

    W3D4_Morning_Algo

---
+++ Week_3_Python/Algos/W3D4.js
@@ -0,0 +1,66 @@
+// add each parenthesis type and compare the two variables
+
+var left_count = 0
+var right_count = 0
+
+
+function parensValid(str) {
+    testArray = []
+    for(i = 0; i < str.length-1; i++){
+        if (str[i]=="("){
+            testArray.push(str[i]);
+        } 
+        else if(str[i]==")"){
+            if(testArray.length==0)return false;
+            else testArray.pop();
+        }
+    }
+    if(testArray.length==0){
+        return true;
+    }
+    else return false;
+}
+
+
+// SAME THING WITH MORE CONDITIONS
+
+const str1 = "W(a{t}s[o(n{ c}o)m]e )h[e{r}e]!";
+const expected1 = true;
+
+const str2 = "D(i{a}l[ t]o)n{e";
+const expected2 = false;
+
+const str3 = "A(1)s[O (n]0{t) 0}k";
+const expected3 = false;
+
+function parensValid(str1) {
+    testArray = []
+    for(i = 0; i < str.length-1; i++){   
+        if (str[i]=="(" || str[i]=="{" || str[i]=="["){;
+          testArray.push(str[i]);
+        } 
+        else if(str[i]==")"){
+          if(testArray.length==0)return false;
+          else if(testArray[testArray.length-2]=="("){
+            testArray.pop();
+          }
+        }
+        else if(str[i]=="}"){
+          if(testArray.length==0)return false;
+          else if(testArray[testArray.length-2]=="{"){
+            testArray.pop();
+          }
+        }        
+        else if(str[i]=="]"){
+          if(testArray.length==0)return false;
+          else if(testArray[testArray.length-2]=="["){
+            testArray.pop();
+          }
+        }
+    }
+    if(testArray.length==0){
+       return true;
+    }
+    else return false;
+  }
+  
